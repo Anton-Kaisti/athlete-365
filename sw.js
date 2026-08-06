03c694e627bbeeb9786481590f461ad2b1b6206e
Anton Kaisti <anton.kaisti@skyfora.com>
2026-08-06 15:06:12 +0300

    Improve jump rope workout prescription

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE = "athlete-365-v20260804-20";
+const CACHE = "athlete-365-v20260806-21";
 const ASSETS = [
   "./",
   "./index.html",
   "./reset-update.html",
-  "./manifest.webmanifest?v=20260804-20",
-  "./src/app.js?v=20260804-20",
+  "./manifest.webmanifest?v=20260806-21",
+  "./src/app.js?v=20260806-21",
   "./src/program.js",
   "./src/state.js",
-  "./src/styles.css?v=20260804-20"
+  "./src/styles.css?v=20260806-21"
 ];
 
 self.addEventListener("install", (event) => {
